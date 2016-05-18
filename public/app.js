886aac6f6d3e75c67ec9bc092c6bd36e38aa28e8
andrewgremlich <andrew.gremlich@gmail.com>
2016-05-18 17:49:23 -0600

    Additional updates to the interface.  Made to be a single page application to load the data from firebase.  Waiting for data to populate firebase so that the goal is easier.  Using dummy data now to model the look.

--- public/app.js
+++ public/app.js
@@ -1,21 +1,24 @@
 (function () {
-
     "use strict";
     var rrRef = new Firebase("https://room-of-requirement.firebaseio.com/"),
         main = document.querySelector("main"),
         nav = document.querySelector("nav"),
-        equellaPara = document.createElement("p"),
         homePara = document.createElement("p"),
-        equellaText = document.createTextNode("Create New Equella Transition Course"),
         homeParaText = document.createTextNode("Home");
 
-    equellaPara.id = "equellaForm";
     homePara.id = "home";
 
-    equellaPara.appendChild(equellaText);
     homePara.appendChild(homeParaText);
     nav.appendChild(homePara);
-    nav.appendChild(equellaPara);
+
+    function getDashboard(e) {
+        var course = e.srcElement.parentNode.id;
+        main.innerHTML = "";
+        rrRef.child("courses/" + course).once("value", function (snap) {
+            var courseData = snap.val();
+            console.log(courseData);
+        });
+    }
 
     function generatePreview(specificCourse, courseKey) {
         var courseContainer = document.createElement("article"),
@@ -24,37 +27,66 @@
             checkerHeader = document.createElement("h2"),
             checkerText = document.createTextNode(specificCourse["employee-checker"]),
             checkbox = document.createElement("input");
-
-        courseContainer.id = courseKey.replace(/\s+/, "");
-        checkbox.type = "checkbox";
-
-        headerCourse.appendChild(headerText);
-        checkerHeader.appendChild(checkerText);
-
+        courseContainer.id = courseKey;
         courseContainer.appendChild(headerCourse);
         courseContainer.appendChild(checkerHeader);
         courseContainer.appendChild(checkbox);
 
+        checkbox.type = "checkbox";
+        headerCourse.appendChild(headerText);
+        checkerHeader.appendChild(checkerText);
         main.appendChild(courseContainer);
+
+        courseContainer.onclick = getDashboard;
     }
 
-    rrRef.child("courses").once("value", function (snap) {
-        var courses = snap.val();
+    /*rrRef.child("mark-report").on("child-added", function (snap, prevKey) {
+        var course = snap.key();
+        console.log(course);
+        rrRef.child("courses/" + course).update({
+            "is-checked-out": false,
+            "employee-checker": "",
+            "links": {
+                "il3-link": "",
+                "equella-link": ""
+            },
+            "dir": {
+                "ocr": {
+                    "name": "",
+                    "email": ""
+                },
+                "course-lead": {
+                    "name": "",
+                    "email": ""
+                }
+            },
+            "exports": {
+                "il3": false,
+                "box": false
+            }
+        });
+    });*/
 
-        snap.forEach(function (csnap) {
-            var specificCourse = csnap.val(),
-                courseKey = csnap.key();
-            console.log(specificCourse, courseKey);
-            generatePreview(specificCourse, courseKey);
-        })
-    });
+    function loadHome() {
+        rrRef.child("courses").once("value", function (snap) {
+            var courses = snap.val();
+            snap.forEach(function (csnap) {
+                var specificCourse = csnap.val(),
+                    courseKey = csnap.key();
+                console.log(specificCourse, courseKey);
+                generatePreview(specificCourse, courseKey);
+            })
+        });
+    }
 
-    equellaPara.onclick = function () {
-        console.log("New Form to be made");
+    window.onload = function () {
+        console.log("onload");
+        loadHome();
     };
 
     homePara.onclick = function () {
         console.log("User wants to go home.");
+        loadHome();
     };
 
 }());
